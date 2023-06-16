2ce222c6511de32c916f284ba74af282c86b1018
rohitpatill <rp2876741@gmail.com>
2023-06-16 08:36:01 +0530

    ToDoListAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
+  title = 'To Do List';
+
+  list:any[]=[];
+
+  addTask(item : String){
+
+    console.warn(item)
+    this.list.push({id:this.list.length, name:item})
+    console.warn(this.list)
+
+     //for clrearing the input text
+    item = '';
+  }
+
+  removeTask(id:number){
+    console.warn(id)
+    this.list = this.list.filter(item=> item.id!== id)
+  }
 }
+
+
+
